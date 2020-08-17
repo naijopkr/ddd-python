a3ec8d0ab862f4ca692bfc0b4a02058a883c7fac
Ariel Barcellos <barcellos.ariel@gmail.com>
2020-08-17 15:11:32 -0500

    implement chapter 1 exercise

--- test_model.py
+++ test_model.py
@@ -1,27 +1,63 @@
-from datetime import date, timedelta
 import pytest
+from datetime import date, timedelta
 
-# from model import ...
+from model import Batch, OrderLine
 
-today = date.today()
-tomorrow = today + timedelta(days=1)
-later = tomorrow + timedelta(days=10)
+
+def make_batch_and_line(sku, batch_qty, line_qty):
+    return (
+        Batch('batch-001', sku, batch_qty, eta=date.today()),
+        OrderLine('order-123', sku, line_qty)
+    )
+
+
+# tomorrow = today + timedelta(days=1)
+# later = tomorrow + timedelta(days=10)
 
 def test_allocating_to_a_batch_reduces_the_available_quantity():
-    pytest.fail('todo')
+    batch, order_line = make_batch_and_line('SMALL-TABLE', 20, 2)
+    batch.allocate(order_line)
+
+    assert batch.available_quantity == 18
 
 def test_can_allocate_if_available_greater_than_required():
-    pytest.fail('todo')
+    large_batch, small_line = make_batch_and_line('ELEGANT-LAMP', 20, 2)
+
+    assert large_batch.can_allocate(small_line)
 
 def test_cannot_allocate_if_available_smaller_than_required():
-    pytest.fail('todo')
+    small_batch, large_line = make_batch_and_line('ELEGANT-LAMP', 2, 20)
+
+    assert small_batch.can_allocate(large_line) is False
 
 def test_can_allocate_if_available_equal_to_required():
-    pytest.fail('todo')
+    batch, line = make_batch_and_line('ELEGANT-LAMP', 2, 2)
 
-def test_prefers_warehouse_batches_to_shipments():
-    pytest.fail('todo')
+    assert batch.can_allocate(line)
 
-def test_prefers_earlier_batches():
-    pytest.fail('todo')
+def test_cannot_allocate_if_skus_do_not_match():
+    batch = Batch('batch-001', 'CHAIR-1', 100, eta=None)
+    different_sku_line = OrderLine('order-001', 'CHAIR-2', 10)
 
+    assert batch.can_allocate(different_sku_line) is False
+
+
+def test_can_only_deallocate_allocated_lines():
+    batch, unacollocate_line = make_batch_and_line('CHAIR-1', 20, 2)
+    batch.deallocate(unacollocate_line)
+
+    assert batch.available_quantity == 20
+
+
+def test_allocation_is_idempotent():
+    batch, line = make_batch_and_line('CHAIR-1', 20, 2)
+    batch.allocate(line)
+    batch.allocate(line)
+
+    assert batch.available_quantity == 18
+
+# def test_prefers_warehouse_batches_to_shipments():
+#     pytest.fail('todo')
+
+# def test_prefers_earlier_batches():
+#     pytest.fail('todo')
